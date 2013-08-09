5e09da76cc34d7241fb1416aa0d55732ada05fd7
Andrew Clark <andy.clark@realvnc.com>
2013-08-09 19:45:05 +0100

    Simple support for clearing all rewrite rules
    Allows all current rewrite rules to be cleared at once. 
    Exposes functionality as DELETE REST call to /:port/rewrite.

--- src/main/java/net/lightbody/bmp/proxy/http/BrowserMobHttpClient.java
+++ src/main/java/net/lightbody/bmp/proxy/http/BrowserMobHttpClient.java
@@ -910,6 +910,10 @@ public class BrowserMobHttpClient {
     public void rewriteUrl(String match, String replace) {
         rewriteRules.add(new RewriteRule(match, replace));
     }
+    
+    public void clearRewriteRules() {
+    	rewriteRules.clear();
+    }
 
     // this method is provided for backwards compatibility before we renamed it to
     // blacklistRequests (note the plural)
