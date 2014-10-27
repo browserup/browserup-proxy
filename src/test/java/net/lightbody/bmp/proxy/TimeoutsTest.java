8ebda4e43f078a151bd301d0be580030f2d2fb1b
Jason Eric Klaes Hoetger <jekh@outlook.com>
2014-10-26 18:12:03 -0700

    Added server shutdown to test

--- src/test/java/net/lightbody/bmp/proxy/TimeoutsTest.java
+++ src/test/java/net/lightbody/bmp/proxy/TimeoutsTest.java
@@ -6,7 +6,10 @@ import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
+
 import static org.junit.Assert.assertEquals;
+
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -32,6 +35,11 @@ public class TimeoutsTest {
 		CloseableHttpResponse response = client.execute(get);
 		
 		assertEquals("Expected HTTP 502 response due to timeout", 502, response.getStatusLine().getStatusCode());
+	}
+	
+	@After
+	public void tearDown() {
+		proxyServer.stop();
 	}
 
 }
