a546e0213f3eebf3df7ff1cac07630581086abc2
Jason Eric Klaes Hoetger <jekh@outlook.com>
2015-01-30 01:20:53 -0800

    Using echo servlet for HttpMethod tests. Added OPTIONS, HEAD, DELETE, PUT, and TRACE tests.

--- browsermob-core/src/test/java/net/lightbody/bmp/proxy/HttpMethodTest.java
+++ browsermob-core/src/test/java/net/lightbody/bmp/proxy/HttpMethodTest.java
@@ -1,29 +1,75 @@
 package net.lightbody.bmp.proxy;
 
-import java.io.IOException;
-
-import net.lightbody.bmp.proxy.test.util.ProxyServerTest;
+import net.lightbody.bmp.proxy.test.util.LocalServerTest;
+import net.lightbody.bmp.proxy.util.IOUtils;
 import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpDelete;
+import org.apache.http.client.methods.HttpHead;
+import org.apache.http.client.methods.HttpOptions;
 import org.apache.http.client.methods.HttpPatch;
-import org.apache.http.util.EntityUtils;
-
-import static org.junit.Assert.assertEquals;
-
+import org.apache.http.client.methods.HttpPut;
+import org.apache.http.client.methods.HttpTrace;
 import org.junit.Ignore;
 import org.junit.Test;
 
-public class HttpMethodTest extends ProxyServerTest {
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class HttpMethodTest extends LocalServerTest {
 
 	@Test
-	@Ignore
-	//TODO: Replace this external call with a call to an internal servlet that will echo the HTTP method in the body, so we can
-	//verify that the proxy handled the PATCH correctly
 	public void testPatch() throws IOException {
-		// using www.yahoo.com, since it currently seems to respond to a PATCH request the same as a GET. [note: this is unstable/unreliable]
-		HttpResponse response = client.execute(new HttpPatch("http://www.yahoo.com"));
-		EntityUtils.consumeQuietly(response.getEntity());
+		HttpResponse response = client.execute(new HttpPatch(getHostnameAndPort() + "/echo"));
+		String body = IOUtils.readFully(response.getEntity().getContent());
 
 		assertEquals("HTTP PATCH request failed", 200, response.getStatusLine().getStatusCode());
+		assertTrue("Server reported HTTP method was not PATCH. Body was: " + body, body.contains("Method: PATCH"));
+	}
+
+	@Test
+	public void testOptions() throws IOException {
+		HttpResponse response = client.execute(new HttpOptions(getHostnameAndPort() + "/echo"));
+		String body = IOUtils.readFully(response.getEntity().getContent());
+
+		assertEquals("HTTP OPTIONS request failed", 200, response.getStatusLine().getStatusCode());
+		assertTrue("Server reported HTTP method was not OPTIONS. Body was: " + body, body.contains("Method: OPTIONS"));
+	}
+
+	@Test
+	public void testHead() throws IOException {
+		HttpResponse response = client.execute(new HttpHead(getHostnameAndPort() + "/echo"));
+		// HEAD responses don't contain entities
+
+		assertEquals("HTTP HEAD request failed", 200, response.getStatusLine().getStatusCode());
+	}
+
+	@Test
+	public void testDelete() throws IOException {
+		HttpResponse response = client.execute(new HttpDelete(getHostnameAndPort() + "/echo"));
+		String body = IOUtils.readFully(response.getEntity().getContent());
+
+		assertEquals("HTTP DELETE request failed", 200, response.getStatusLine().getStatusCode());
+		assertTrue("Server reported HTTP method was not DELETE. Body was: " + body, body.contains("Method: DELETE"));
+	}
+
+	@Test
+	public void testPut() throws IOException {
+		HttpResponse response = client.execute(new HttpPut(getHostnameAndPort() + "/echo"));
+		String body = IOUtils.readFully(response.getEntity().getContent());
+
+		assertEquals("HTTP PUT request failed", 200, response.getStatusLine().getStatusCode());
+		assertTrue("Server reported HTTP method was not PUT. Body was: " + body, body.contains("Method: PUT"));
+	}
+
+	@Test
+	public void testTrace() throws IOException {
+		HttpResponse response = client.execute(new HttpTrace(getHostnameAndPort() + "/echo"));
+		String body = IOUtils.readFully(response.getEntity().getContent());
+
+		assertEquals("HTTP TRACE request failed", 200, response.getStatusLine().getStatusCode());
+		assertTrue("Server reported HTTP method was not TRACE. Body was: " + body, body.contains("Method: TRACE"));
 	}
 	
 }
