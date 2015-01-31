34f7fce8b474a44cca429506b6eefb9047b5427f
Jason Eric Klaes Hoetger <jekh@outlook.com>
2015-01-30 17:50:23 -0800

    Added a zero ttl non-expiring-proxy test

--- browsermob-rest/src/test/java/net/lightbody/bmp/proxy/ExpiringProxyTest.java
+++ browsermob-rest/src/test/java/net/lightbody/bmp/proxy/ExpiringProxyTest.java
@@ -11,25 +11,20 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 
 public class ExpiringProxyTest {
-    private ProxyManager proxyManager;
-
-    @Before
-    public void setUp() {
-        int minPort = new Random().nextInt(50000) + 10000;
-
-        proxyManager = new ProxyManager(new Provider<ProxyServer>() {
-                @Override
-                public ProxyServer get() {
-                    return new ProxyServer();
-                }
-            },
-            minPort,
-            minPort + 100,
-            2);
-    }
-
     @Test
     public void testExpiredProxyStops() throws InterruptedException {
+        int minPort = new Random().nextInt(50000) + 10000;
+
+        ProxyManager proxyManager = new ProxyManager(new Provider<ProxyServer>() {
+            @Override
+            public ProxyServer get() {
+                return new ProxyServer();
+            }
+        },
+                minPort,
+                minPort + 100,
+                2);
+
         ProxyServer proxy = proxyManager.create(Collections.<String, String>emptyMap());
         int port = proxy.getPort();
 
@@ -46,6 +41,38 @@ public class ExpiringProxyTest {
         ProxyServer expiredProxy = proxyManager.get(port);
 
         assertNull("ProxyManager did not expire proxy as expected", expiredProxy);
+    }
+
+    @Test
+    public void testZeroTtlProxyDoesNotExpire() throws InterruptedException {
+        int minPort = new Random().nextInt(50000) + 10000;
+
+        ProxyManager proxyManager = new ProxyManager(new Provider<ProxyServer>() {
+            @Override
+            public ProxyServer get() {
+                return new ProxyServer();
+            }
+        },
+                minPort,
+                minPort + 100,
+                0);
+
+        ProxyServer proxy = proxyManager.create(Collections.<String, String>emptyMap());
+        int port = proxy.getPort();
+
+        ProxyServer retrievedProxy = proxyManager.get(port);
+
+        assertEquals("ProxyManager did not return the expected proxy instance", proxy, retrievedProxy);
+
+        Thread.sleep(2500);
+
+        // explicitly create a new proxy to cause a write to the cache. cleanups happen on "every" write and "occasional" reads, so force a cleanup by writing.
+        int newPort = proxyManager.create(Collections.<String, String>emptyMap()).getPort();
+        proxyManager.delete(newPort);
+
+        ProxyServer nonExpiredProxy = proxyManager.get(port);
+
+        assertEquals("ProxyManager did not return the expected proxy instance", proxy, nonExpiredProxy);
     }
 
 }
