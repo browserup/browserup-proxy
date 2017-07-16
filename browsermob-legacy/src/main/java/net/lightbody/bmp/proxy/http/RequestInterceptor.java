2c7f1d8ce2efdae93846a16ba420667386b4a1d2
Jason Hoetger <jekh@outlook.com>
2017-07-16 14:27:18 -0700

    Removed browsermob-legacy source

--- browsermob-legacy/src/main/java/net/lightbody/bmp/proxy/http/RequestInterceptor.java
+++
@@ -1,7 +0,0 @@
-package net.lightbody.bmp.proxy.http;
-
-import net.lightbody.bmp.core.har.Har;
-
-public interface RequestInterceptor {
-    void process(BrowserMobHttpRequest request, Har har);
-}
--- browsermob-legacy/src/main/java/net/lightbody/bmp/proxy/http/ResponseInterceptor.java
+++
@@ -1,7 +0,0 @@
-package net.lightbody.bmp.proxy.http;
-
-import net.lightbody.bmp.core.har.Har;
-
-public interface ResponseInterceptor {
-    void process(BrowserMobHttpResponse response, Har har);
-}
