8b707cecc691fbe6fc7cefa3058a59a1a3beaf3d
kirill.turutin <kiturutin@gmail.com>
2020-04-07 16:34:47 +0300

    auto scanning for managing endpoints in custom addons

--- browserup-proxy-core/src/main/resources/mitmproxy/bu_addons_manager.py
+++ browserup-proxy-core/src/main/resources/mitmproxy/bu_addons_manager.py
@@ -1,27 +1,54 @@
 import falcon
+import _thread
 
-from examples.complex.har_dump import HarDumpAddonResource
+from mitmproxy import ctx
+
 from wsgiref.simple_server import make_server
 
-addons_manager = {}
-
-def load(l):
-    global addons_manager
-    addons_manager = l.master.addons
+initialized = False
 
 
-def configure(updated):
-    start_falcon()
+def running():
+    if not initialized and is_script_loader_initialized():
+        resources = get_resources()
+        _thread.start_new_thread(start_falcon, tuple([resources]))
 
 
-async def start_falcon():
+def is_script_loader_initialized():
+    script_loader = ctx.master.addons.get("scriptloader")
+
+    for custom_addon in script_loader.addons:
+        if len(custom_addon.addons) == 0:
+            return False
+
+    return True
+
+
+def get_resources():
+    script_loader = ctx.master.addons.get("scriptloader")
+    resources = []
+
+    for custom_addon in script_loader.addons:
+        custom_loaded_addon = custom_addon.addons[0]
+        if hasattr(custom_loaded_addon, "addons"):
+            for ca in custom_loaded_addon.addons:
+                if hasattr(ca, "get_resource"):
+                    resources.append(getattr(ca, "get_resource")())
+        if hasattr(custom_loaded_addon, "get_resource"):
+            resources.append(getattr(custom_loaded_addon, "get_resource")())
+
+    return resources
+
+
+def start_falcon(resources):
     app = falcon.API()
-    app.add_route('/har_addon/{method_name}', HarDumpAddonResource())
+    for resource in resources:
+        app.add_route("/" + resource.addon_path() + "/{method_name}", resource)
 
     with make_server('', 8088, app) as httpd:
         print('Serving on port 8088...')
         httpd.serve_forever()
 
+
 def done():
     print("done")
-
--- browserup-proxy-core/src/main/resources/mitmproxy/har_dump.py
+++ browserup-proxy-core/src/main/resources/mitmproxy/har_dump.py
@@ -10,10 +10,7 @@ mitmdump -s ./har_dump.py --set hardump=./dump.zhar
 
 import json
 import base64
-import zlib
-import os
-import typing  # noqa
-
+import typing
 import tempfile
 
 from datetime import datetime
@@ -21,15 +18,10 @@ from datetime import timezone
 
 import falcon
 
-import mitmproxy
-
-from mitmproxy import connections  # noqa
+from mitmproxy import connections
 from mitmproxy import version
-from mitmproxy import ctx
 from mitmproxy.utils import strutils
 from mitmproxy.net.http import cookies
-
-HAR: typing.Dict = {}
 
 # A list of server seen till now is maintained so we can avoid
 # using 'connect' time for entries that use an existing connection.
@@ -39,11 +31,20 @@ addonsManager = {}
 
 
 class HarDumpAddonResource:
+
+    def addon_path(self):
+        return "har"
+
+    def __init__(self, harDumpAddOn):
+        self.num = 0
+        self.name = "hardump"
+        self.harDumpAddOn = harDumpAddOn
+
     def on_get(self, req, resp, method_name):
         getattr(self, "on_" + method_name)(req, resp)
 
     def on_get_har(self, req, resp):
-        har_file = save_current_har()
+        har_file = self.harDumpAddOn.save_current_har()
 
         resp.status = falcon.HTTP_200
         resp.body = json.dumps({
@@ -51,217 +52,197 @@ class HarDumpAddonResource:
         }, ensure_ascii=False)
 
 
-def load(l):
-    global addonsManager
-    addonsManager = l.master.addons
+class HarDumpAddOn:
 
+    def __init__(self):
+        self.num = 0
+        self.har = {}
 
-def reset_har():
-    HAR.update({
-        "log": {
-            "version": "1.2",
-            "creator": {
-                "name": "mitmproxy har_dump",
-                "version": "0.1",
-                "comment": "mitmproxy version %s" % version.MITMPROXY
-            },
-            "entries": []
-        }
-    })
+    def reset_har(self):
+        self.har.update({
+            "log": {
+                "version": "1.2",
+                "creator": {
+                    "name": "mitmproxy har_dump",
+                    "version": "0.1",
+                    "comment": "mitmproxy version %s" % version.MITMPROXY
+                },
+                "entries": []
+            }
+        })
 
+    def configure(self, updated):
+        self.reset_har()
 
-def configure(updated):
-    reset_har()
+    def get_resource(self):
+        return HarDumpAddonResource(self)
 
+    def response(self, flow):
+        """
+             Called when a server response has been received.
+        """
+        # -1 indicates that these values do not apply to current request
+        ssl_time = -1
+        connect_time = -1
 
-def response(flow):
-    """
-         Called when a server response has been received.
-      """
+        if flow.server_conn and flow.server_conn not in SERVERS_SEEN:
+            connect_time = (flow.server_conn.timestamp_tcp_setup -
+                            flow.server_conn.timestamp_start)
 
-    # -1 indicates that these values do not apply to current request
-    ssl_time = -1
-    connect_time = -1
+            if flow.server_conn.timestamp_tls_setup is not None:
+                ssl_time = (flow.server_conn.timestamp_tls_setup -
+                            flow.server_conn.timestamp_tcp_setup)
 
-    if flow.server_conn and flow.server_conn not in SERVERS_SEEN:
-        connect_time = (flow.server_conn.timestamp_tcp_setup -
-                        flow.server_conn.timestamp_start)
+            SERVERS_SEEN.add(flow.server_conn)
 
-        if flow.server_conn.timestamp_tls_setup is not None:
-            ssl_time = (flow.server_conn.timestamp_tls_setup -
-                        flow.server_conn.timestamp_tcp_setup)
-
-        SERVERS_SEEN.add(flow.server_conn)
-
-    # Calculate raw timings from timestamps. DNS timings can not be calculated
-    # for lack of a way to measure it. The same goes for HAR blocked.
-    # mitmproxy will open a server connection as soon as it receives the host
-    # and port from the client connection. So, the time spent waiting is actually
-    # spent waiting between request.timestamp_end and response.timestamp_start
-    # thus it correlates to HAR wait instead.
-    timings_raw = {
-        'send': flow.request.timestamp_end - flow.request.timestamp_start,
-        'receive': flow.response.timestamp_end - flow.response.timestamp_start,
-        'wait': flow.response.timestamp_start - flow.request.timestamp_end,
-        'connect': connect_time,
-        'ssl': ssl_time,
-    }
-
-    # HAR timings are integers in ms, so we re-encode the raw timings to that format.
-    timings = {
-        k: int(1000 * v) if v != -1 else -1
-        for k, v in timings_raw.items()
-    }
-
-    # full_time is the sum of all timings.
-    # Timings set to -1 will be ignored as per spec.
-    full_time = sum(v for v in timings.values() if v > -1)
-
-    started_date_time = datetime.fromtimestamp(flow.request.timestamp_start,
-                                               timezone.utc).isoformat()
-
-    # Response body size and encoding
-    response_body_size = len(
-        flow.response.raw_content) if flow.response.raw_content else 0
-    response_body_decoded_size = len(
-        flow.response.content) if flow.response.content else 0
-    response_body_compression = response_body_decoded_size - response_body_size
-
-    entry = {
-        "startedDateTime": started_date_time,
-        "time": full_time,
-        "request": {
-            "method": flow.request.method,
-            "url": flow.request.url,
-            "httpVersion": flow.request.http_version,
-            "cookies": format_request_cookies(flow.request.cookies.fields),
-            "headers": name_value(flow.request.headers),
-            "queryString": name_value(flow.request.query or {}),
-            "headersSize": len(str(flow.request.headers)),
-            "bodySize": len(flow.request.content),
-        },
-        "response": {
-            "status": flow.response.status_code,
-            "statusText": flow.response.reason,
-            "httpVersion": flow.response.http_version,
-            "cookies": format_response_cookies(flow.response.cookies.fields),
-            "headers": name_value(flow.response.headers),
-            "content": {
-                "size": response_body_size,
-                "compression": response_body_compression,
-                "mimeType": flow.response.headers.get('Content-Type', '')
-            },
-            "redirectURL": flow.response.headers.get('Location', ''),
-            "headersSize": len(str(flow.response.headers)),
-            "bodySize": response_body_size,
-        },
-        "cache": {},
-        "timings": timings,
-    }
-
-    # Store binary data as base64
-    if strutils.is_mostly_bin(flow.response.content):
-        entry["response"]["content"]["text"] = base64.b64encode(
-            flow.response.content).decode()
-        entry["response"]["content"]["encoding"] = "base64"
-    else:
-        entry["response"]["content"]["text"] = flow.response.get_text(
-            strict=False)
-
-    if flow.request.method in ["POST", "PUT", "PATCH"]:
-        params = [
-            {"name": a, "value": b}
-            for a, b in flow.request.urlencoded_form.items(multi=True)
-        ]
-        entry["request"]["postData"] = {
-            "mimeType": flow.request.headers.get("Content-Type", ""),
-            "text": flow.request.get_text(strict=False),
-            "params": params
+        # Calculate raw timings from timestamps. DNS timings can not be calculated
+        # for lack of a way to measure it. The same goes for HAR blocked.
+        # mitmproxy will open a server connection as soon as it receives the host
+        # and port from the client connection. So, the time spent waiting is actually
+        # spent waiting between request.timestamp_end and response.timestamp_start
+        # thus it correlates to HAR wait instead.
+        timings_raw = {
+            'send': flow.request.timestamp_end - flow.request.timestamp_start,
+            'receive': flow.response.timestamp_end - flow.response.timestamp_start,
+            'wait': flow.response.timestamp_start - flow.request.timestamp_end,
+            'connect': connect_time,
+            'ssl': ssl_time,
         }
 
-    if flow.server_conn.connected():
-        entry["serverIPAddress"] = str(flow.server_conn.ip_address[0])
+        # HAR timings are integers in ms, so we re-encode the raw timings to that format.
+        timings = {
+            k: int(1000 * v) if v != -1 else -1
+            for k, v in timings_raw.items()
+        }
 
-    HAR["log"]["entries"].append(entry)
+        # full_time is the sum of all timings.
+        # Timings set to -1 will be ignored as per spec.
+        full_time = sum(v for v in timings.values() if v > -1)
 
+        started_date_time = datetime.fromtimestamp(flow.request.timestamp_start,
+                                                   timezone.utc).isoformat()
 
-def save_current_har():
-    json_dump: str = json.dumps(HAR, indent=2)
+        # Response body size and encoding
+        response_body_size = len(
+            flow.response.raw_content) if flow.response.raw_content else 0
+        response_body_decoded_size = len(
+            flow.response.content) if flow.response.content else 0
+        response_body_compression = response_body_decoded_size - response_body_size
 
-    tmp_file = tempfile.NamedTemporaryFile(mode="wb", prefix="har_dump_", delete=False)
+        entry = {
+            "startedDateTime": started_date_time,
+            "time": full_time,
+            "request": {
+                "method": flow.request.method,
+                "url": flow.request.url,
+                "httpVersion": flow.request.http_version,
+                "cookies": self.format_request_cookies(
+                    flow.request.cookies.fields),
+                "headers": self.name_value(flow.request.headers),
+                "queryString": self.name_value(flow.request.query or {}),
+                "headersSize": len(str(flow.request.headers)),
+                "bodySize": len(flow.request.content),
+            },
+            "response": {
+                "status": flow.response.status_code,
+                "statusText": flow.response.reason,
+                "httpVersion": flow.response.http_version,
+                "cookies": self.format_response_cookies(
+                    flow.response.cookies.fields),
+                "headers": self.name_value(flow.response.headers),
+                "content": {
+                    "size": response_body_size,
+                    "compression": response_body_compression,
+                    "mimeType": flow.response.headers.get('Content-Type', '')
+                },
+                "redirectURL": flow.response.headers.get('Location', ''),
+                "headersSize": len(str(flow.response.headers)),
+                "bodySize": response_body_size,
+            },
+            "cache": {},
+            "timings": timings,
+        }
 
-    raw: bytes = json_dump.encode()
-
-    tmp_file.write(raw)
-    tmp_file.flush()
-    tmp_file.close()
-
-    mitmproxy.ctx.log(
-        "HAR dump finished (wrote %s bytes to file)" % len(json_dump))
-
-    return tmp_file
-
-
-def done():
-    """
-        Called once on script shutdown, after any other events.
-    """
-    if ctx.options.hardump:
-        json_dump: str = json.dumps(HAR, indent=2)
-
-        if ctx.options.hardump == '-':
-            mitmproxy.ctx.log(json_dump)
+        # Store binary data as base64
+        if strutils.is_mostly_bin(flow.response.content):
+            entry["response"]["content"]["text"] = base64.b64encode(
+                flow.response.content).decode()
+            entry["response"]["content"]["encoding"] = "base64"
         else:
-            raw: bytes = json_dump.encode()
-            if ctx.options.hardump.endswith('.zhar'):
-                raw = zlib.compress(raw, 9)
+            entry["response"]["content"]["text"] = flow.response.get_text(
+                strict=False)
 
-            with open(os.path.expanduser(ctx.options.hardump), "wb") as f:
-                f.write(raw)
+        if flow.request.method in ["POST", "PUT", "PATCH"]:
+            params = [
+                {"name": a, "value": b}
+                for a, b in flow.request.urlencoded_form.items(multi=True)
+            ]
+            entry["request"]["postData"] = {
+                "mimeType": flow.request.headers.get("Content-Type", ""),
+                "text": flow.request.get_text(strict=False),
+                "params": params
+            }
 
-            mitmproxy.ctx.log(
-                "HAR dump finished (wrote %s bytes to file)" % len(json_dump))
+        if flow.server_conn.connected():
+            entry["serverIPAddress"] = str(flow.server_conn.ip_address[0])
+
+        self.har["log"]["entries"].append(entry)
+
+    def save_current_har(self):
+        json_dump: str = json.dumps(self.har, indent=2)
+
+        tmp_file = tempfile.NamedTemporaryFile(mode="wb", prefix="har_dump_",
+                                               delete=False)
+
+        raw: bytes = json_dump.encode()
+
+        tmp_file.write(raw)
+        tmp_file.flush()
+        tmp_file.close()
+
+        return tmp_file
+
+    def format_cookies(self, cookie_list):
+        rv = []
+
+        for name, value, attrs in cookie_list:
+            cookie_har = {
+                "name": name,
+                "value": value,
+            }
+
+            # HAR only needs some attributes
+            for key in ["path", "domain", "comment"]:
+                if key in attrs:
+                    cookie_har[key] = attrs[key]
+
+            # These keys need to be boolean!
+            for key in ["httpOnly", "secure"]:
+                cookie_har[key] = bool(key in attrs)
+
+            # Expiration time needs to be formatted
+            expire_ts = cookies.get_expiration_ts(attrs)
+            if expire_ts is not None:
+                cookie_har["expires"] = datetime.fromtimestamp(expire_ts,
+                                                               timezone.utc).isoformat()
+
+            rv.append(cookie_har)
+
+        return rv
+
+    def format_request_cookies(self, fields):
+        return self.format_cookies(cookies.group_cookies(fields))
+
+    def format_response_cookies(self, fields):
+        return self.format_cookies((c[0], c[1][0], c[1][1]) for c in fields)
+
+    def name_value(self, obj):
+        """
+            Convert (key, value) pairs to HAR format.
+        """
+        return [{"name": k, "value": v} for k, v in obj.items()]
 
 
-def format_cookies(cookie_list):
-    rv = []
-
-    for name, value, attrs in cookie_list:
-        cookie_har = {
-            "name": name,
-            "value": value,
-        }
-
-        # HAR only needs some attributes
-        for key in ["path", "domain", "comment"]:
-            if key in attrs:
-                cookie_har[key] = attrs[key]
-
-        # These keys need to be boolean!
-        for key in ["httpOnly", "secure"]:
-            cookie_har[key] = bool(key in attrs)
-
-        # Expiration time needs to be formatted
-        expire_ts = cookies.get_expiration_ts(attrs)
-        if expire_ts is not None:
-            cookie_har["expires"] = datetime.fromtimestamp(expire_ts,
-                                                           timezone.utc).isoformat()
-
-        rv.append(cookie_har)
-
-    return rv
-
-
-def format_request_cookies(fields):
-    return format_cookies(cookies.group_cookies(fields))
-
-
-def format_response_cookies(fields):
-    return format_cookies((c[0], c[1][0], c[1][1]) for c in fields)
-
-
-def name_value(obj):
-    """
-        Convert (key, value) pairs to HAR format.
-    """
-    return [{"name": k, "value": v} for k, v in obj.items()]
+addons = [
+    HarDumpAddOn()
+]
